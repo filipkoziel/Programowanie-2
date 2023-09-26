88e10e53e2016be0d6d0283a367c441a3f4d3914
fil <nhhj>
2023-09-26 16:16:53 +0200

    Completed tasks 6/9

--- firsrconsoleapplication/ConsoleApplication1/ConsoleApplication1.cpp
+++ firsrconsoleapplication/ConsoleApplication1/ConsoleApplication1.cpp
@@ -46,22 +46,28 @@ Podstawy programowania:
 void task1()
 {
     double fstNumber, sndNumber;
-    cout << "imput first number \n";
+    cout << "input first number \n";
     cin >> fstNumber;
-    cout << "imput second number \n";
+    cout << "input second number \n";
     cin >> sndNumber;
-    cout << "arithmetic average: " << (fstNumber + sndNumber) / 2;
+
+    double average = (fstNumber + sndNumber) / 2;
+
+    cout << "arithmetic average: " << average << "\n";
 }
 
 //2. Program obliczaj¹cy pole prostok¹ta.
 void task2()
 {
     double a, b;
-    cout << "imput rectangle side a \n";
+    cout << "input rectangle side a \n";
     cin >> a;
-    cout << "imput rectangle side b \n";
+    cout << "input rectangle side b \n";
     cin >> b;
-    cout << a * b;
+
+    double volume = a * b;
+
+    cout << "volume of ractangle:" << volume << "\n";
 }
 
 //3. Program obliczaj¹cy objêtoœæ sto¿ka.
@@ -69,14 +75,14 @@ void task3()
 {
     double radius, height;
 
-    cout << "imput cone radius \n";
+    cout << "input cone radius \n";
     cin >> radius;
-    cout << "imput cone height \n";
+    cout << "input cone height \n";
     cin >> height;
 
     double coneVolume = (1.0 / 3.0) * 3.14 * pow(radius, 2.0) * height;
 
-    cout << "cone volume = \n" << coneVolume;
+    cout << "cone volume: " << coneVolume << "\n";
 }
 
 //4. Program obliczaj¹cy pole ko³a.
@@ -84,7 +90,7 @@ void task4()
 {
     double radius;
 
-    cout << "imput circle radius \n";
+    cout << "input circle radius \n";
     cin >> radius;
 
     double circleVolume = 3.14 * pow(radius, 2.0);
@@ -97,9 +103,9 @@ void task5()
 {
     double a, b;
 
-    cout << "imput a \n";
+    cout << "input a \n";
     cin >> a;
-    cout << "imput b \n";
+    cout << "input b \n";
     cin >> b;
 
     double value = pow(a, 2.0) + pow(b, 2.0);
@@ -107,12 +113,82 @@ void task5()
     cout << "value of a^2 + b^2: " << value;
 }
 
+//6. Program obliczaj¹cy pole trójk¹ta o podstawie b i wysokoœci h
+void task6()
+{
+    double base, height;
+
+    cout << "input base \n";
+    cin >> base;
+    cout << "input height \n";
+    cin >> height;
+
+    double Volume = (base * height) / 2;
+
+    cout << "the volume of a triangle with a base of " << base << " and a height of " << height << " is: " << Volume << "\n";
+}
+
+//7. Program obliczaj¹cy objêtoœæ kuli o promieniu r
+void task7()
+{
+    double radius;
+
+    cout << "input radius \n";
+    cin >> radius;
+
+    double volume = (4.0 / 3.0) * M_PI * pow(radius, 3.0);
+
+    cout << "The volume of a sphere with the radius of " << radius << " is: " << volume << "\n";
+}
+
+// 8. Program obliczaj¹cy pole trapezu o podstawach a i b oraz wysokoœci h
+void task8()
+{
+    double aBase, bBase, height;
+
+    cout << "input base a \n";
+    cin >> aBase;    
+    cout << "input base b \n";
+    cin >> bBase;    
+    cout << "input height \n";
+    cin >> height;
+
+    double volume = ((aBase + bBase) * height) / 2;
+
+    cout << "The volume of a trapeze with bases of " << aBase << " and " << bBase << " is: " << volume << "\n";
+}
+
+// 9. Program obliczaj¹cy œredni¹ wa¿on¹ trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+void task9()
+{
+    double aNumber, bNumber, cNumber, aWeight, bWeight, cWeight;
+
+    aNumber = 8.0;
+    bNumber = 2.0;
+    cNumber = 4.0;
+
+    cout << "input weight of first number with value 8 \n";
+    cin >> aWeight;
+    cout << "input weight of second number with value 2 \n";
+    cin >> bWeight;
+    cout << "input weight of third number with value 4 \n";
+    cin >> cWeight;
+
+    double weightedAverage = ((aNumber * aWeight) + (bNumber * bWeight) + (cNumber * cWeight)) / 3;
+
+    cout << "The weighted average of those numbers with those weights is: " << weightedAverage << "\n";
+}
+
 int main()
 {
     //test();
-    //task1();
-    //task2();
-    //task3();
-    //task4();
+    task1();
+    task2();
+    task3();
+    task4();
     task5();
+    task6();
+    task7();
+    task8();
+    task9();
 }
