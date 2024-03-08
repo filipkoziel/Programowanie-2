a2aca466e8e9ada6b4df3798ee65c3f5c103dcd2
fil <nhhj>
2024-03-08 13:17:15 +0100

    rgaegaeg

--- firsrconsoleapplication/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
+++ firsrconsoleapplication/FirstObjectConsoleApplication/FirstObjectConsoleApplication.cpp
@@ -3,16 +3,6 @@
 
 using namespace std;
 
-/*
-Zdefiniuj klase opisuj¹c¹ prostok¹t. Mo¿liwoœci klasy:
-* konstruktory
-* metoda która zwórci obwód prostok¹ta
-* metoda która pole prostok¹ta
-* metodê która poka¿e informacje o prostok¹cie.
-
-Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
-*/
-
 class Point
 {
 private:
@@ -73,6 +63,16 @@ public:
 	}
 };
 
+/*
+Zdefiniuj klase opisuj¹c¹ prostok¹t. Mo¿liwoœci klasy:
+* konstruktory
+* metoda która zwórci obwód prostok¹ta
+* metoda która pole prostok¹ta
+* metodê która poka¿e informacje o prostok¹cie.
+
+Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
+*/
+
 class Rectangle
 {
 private:
@@ -94,7 +94,7 @@ public:
 
 	~Rectangle()
 	{
-		cout << "koniec :p\n";
+		cout << "dekonstruktuje...\n";
 	}
 
 	int getCircumference()
@@ -115,9 +115,91 @@ public:
 	}
 };
 
+/*
+ZADANIE
+Zdefiniuj klase opisuj¹ce konto bankowe. Mo¿liwoœci klasy:
+* konstruktory
+* metoda która dokona wp³aty na konto
+* metoda która wyp³aty z konta
+* metoda która wykona przelew na inne konto
+* metodê która poka¿e informacje o koncie.
+
+Napisz program który zaprezentuje mo¿liwoœci obiektu na podstawie tej klasy.
+*/
+
+class BankAcc
+{
+private:
+	double money;
+
+public:
+	BankAcc()
+	{
+		money = 0;
+	}
+
+	~BankAcc()
+	{
+		cout << "closing your bank account... thank you for using our services\n";
+	}
+
+	void DepositOrPayment(double change)
+	{
+		if (change >= 0)
+			money += change;
+		else if (change < 0 && change < money)
+			money -= change;
+	}
+
+	double TransferMoney(double amountOfMoney)
+	{
+		if (amountOfMoney <= money && amountOfMoney > 0)
+		{
+			money -= amountOfMoney;
+			return amountOfMoney;
+		}
+		else if (amountOfMoney > money || amountOfMoney < 0)
+		{
+			return 0;
+		}
+	}
+
+	void AccountInfo()
+	{
+		cout << "money in account: " << money;
+	}
+
+};
+
 int main()
 {
-	Rectangle firstRectangle(12, 6);
+	setlocale(LC_ALL, "");
 
-	firstRectangle.getInfo();
+	BankAcc firstbank, secondbank;
+
+	cout << "pierwsze konto: ";
+	firstbank.AccountInfo();
+	cout << "\ndrugie konto: ";
+	secondbank.AccountInfo();
+
+	cout << "\n\n";
+
+	firstbank.DepositOrPayment(10);
+	secondbank.DepositOrPayment(100);
+
+	cout << "pierwsze konto: ";
+	firstbank.AccountInfo();
+	cout << "\ndrugie konto: ";
+	secondbank.AccountInfo();
+
+	cout << "\n\n";
+
+	firstbank.DepositOrPayment(secondbank.TransferMoney(110));
+
+	cout << "pierwsze konto: ";
+	firstbank.AccountInfo();
+	cout << "\ndrugie konto: ";
+	secondbank.AccountInfo();
+
+	cout << "\n";
 }
