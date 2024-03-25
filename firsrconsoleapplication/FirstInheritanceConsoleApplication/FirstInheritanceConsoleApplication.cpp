c9b19233b3975f41b93e54a726f5f4989f1919f4
fil <nhhj>
2024-03-25 11:37:49 +0100

    agasg

---
+++ firsrconsoleapplication/FirstInheritanceConsoleApplication/FirstInheritanceConsoleApplication.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+
+using namespace std;
+
+class C
+{
+protected:
+    int fieldA;
+    string name;
+
+public:
+
+    C()
+    {
+        fieldA = 15;
+        name = "C";
+    }
+
+    C(int a, string n)
+    {
+        fieldA = a;
+        name = n;
+    }
+
+    int GetFieldA()
+    {
+        return fieldA;
+    }
+
+    void ShowInfo()
+    {
+        std::cout << "Informacja na temat klasy "<< name <<"\n";
+        std::cout << "Field A = " << fieldA << "\n";
+    }
+};
+
+class A : public C
+{
+private:
+
+public:
+    A():C(5, "A")
+    {
+
+    }
+
+    /*
+        void ShowInfo()
+    {
+        std::cout << "Informacja na temat klasy A\n";
+        std::cout << "Field A = " << fieldA << "\n";
+    }
+    */
+};
+
+class B : public C
+{
+private:
+    //int fieldA;
+    int fieldB;
+
+public:
+    B(): C(10, "B")
+    {
+        fieldB = 4;
+    }
+
+    /*int GetFieldA()
+    {
+        return fieldA;
+    }*/
+
+    int GetFieldB()
+    {
+        return fieldB;
+    }
+
+    /*
+    void ShowInfo()
+    {
+        std::cout << "Informacja na temat klasy B\n";
+        std::cout << "Field A = " << fieldA << "\n";
+        std::cout << "Field B = " << fieldB << "\n";
+    }
+    */
+
+};
+
+int main()
+{
+    A a;
+    a.ShowInfo();
+
+    B b;
+    b.ShowInfo();
+
+    C c;
+    c.ShowInfo();
+}
