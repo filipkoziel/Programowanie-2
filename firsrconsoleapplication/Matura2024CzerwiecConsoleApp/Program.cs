e2ade1da3721d059d14f2302914456afdbb00feb
q <q>
2025-02-18 15:13:21 +0100

    gasgasg

--- firsrconsoleapplication/Matura2024CzerwiecConsoleApp/Program.cs
+++ firsrconsoleapplication/Matura2024CzerwiecConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿void oddShortCut(int number)
+﻿string oddShortCut(int number)
 {
     List<int> odds = new List<int>();
     int oddShort = 0;
@@ -8,13 +8,38 @@
         number = (number / 10);
         odds = odds.Where(x => x % 2 != 0).ToList();
     }
-    foreach (int i in odds)
-    {
-        Console.Write(i);
-    }
+    string result = String.Join("", odds);
+
+    return result;
 }
 
-int number = int.Parse(Console.ReadLine());
+int testNumber = int.Parse(Console.ReadLine());
 
-oddShortCut(number);
+// 3.1 oddShortCut
 
+Console.WriteLine($"{testNumber} - {oddShortCut(testNumber)}");
+
+// 3.2 
+
+StreamReader steamreader = new StreamReader("skrot.txt");
+
+string strNumberFromFile;
+List<int> numbers = new List<int>();
+
+while ((strNumberFromFile = steamreader.ReadLine()) != null)
+{
+    numbers.Add(int.Parse(strNumberFromFile));
+}
+steamreader.Close();
+
+foreach (int number in numbers)
+{
+    int maxnumber = 0;
+    int countImpossible = 0;
+    int oddShortNumber = int.Parse(oddShortCut(number));
+
+    if (oddShortNumber == 0)
+        countImpossible++;
+    if (oddShortNumber )
+
+}
