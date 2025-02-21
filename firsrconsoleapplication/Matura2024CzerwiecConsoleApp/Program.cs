0ae5f71ae1c4e0f7599b67b150f372f40c38fc32
q <q>
2025-02-21 11:37:40 +0100

    gwagwag

--- firsrconsoleapplication/Matura2024CzerwiecConsoleApp/Program.cs
+++ firsrconsoleapplication/Matura2024CzerwiecConsoleApp/Program.cs
@@ -1,31 +1,34 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 
 int oddShortCut(int number)
 {
     List<int> odds = new List<int>();
     int oddShort = 0;
+    int result = 0;
     for (int i = 10; i<100; i++)
     {
         odds.Add(number % 10);
         number = (number / 10);
-        odds = odds.Where(x => x % 2 != 0).ToList();
     }
-    int result = 0;
-    foreach (int entry in odds)
+
+    odds = odds.Where(x => x % 2 != 0).Reverse().ToList();
+
+    foreach (int digit in odds)
     {
-        result = 10 * result + entry;
+        result = 10 * result + digit;
     }
 
     return result;
 }
 
-//int testNumber = int.Parse(Console.ReadLine());
+
 
 // 3.1 oddShortCut
 
+//int testNumber = int.Parse(Console.ReadLine());
+
 //Console.WriteLine($"{testNumber} - {oddShortCut(testNumber)}");
-
-
 
 // 3.2 
 
@@ -40,7 +43,8 @@ while ((strNumberFromFile = steamreader.ReadLine()) != null)
 }
 steamreader.Close();
 
-int maxnumber = 0;
+
+int maxImpossibleNumber = 0;
 int countImpossible = 0;
 foreach (string strNumber in strNumbers)
 {
@@ -48,13 +52,40 @@ foreach (string strNumber in strNumbers)
     if (oddShortCut(number) == 0)
     { 
         countImpossible++;
-        if (number > maxnumber)
-            maxnumber = number;
+        if (number > maxImpossibleNumber)
+            maxImpossibleNumber = number;
     }
 
 }
 
-Console.WriteLine($"{countImpossible} - {maxnumber}");
+Console.WriteLine($"Liczb które nie mają nieparzystego skrutu jest: {countImpossible}, a najwięszą z nich jest: {maxImpossibleNumber}");
 
-// 3.2
+// 3.3
 
+StreamReader steamreader2 = new StreamReader("skrot2.txt");
+
+string strNumberFromFile2;
+List<string> strNumbers2 = new List<string>();
+
+while ((strNumberFromFile2 = steamreader2.ReadLine()) != null)
+{
+    strNumbers2.Add(strNumberFromFile2);
+}
+steamreader2.Close();
+
+
+foreach (string strNumber2 in strNumbers2)
+{
+    int number = int.Parse(strNumber2);
+    int a = number;
+    int b = oddShortCut(number);
+    int temp;
+    while (b != 0)
+    {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    if (a == 7)
+    Console.WriteLine(number);
+}
