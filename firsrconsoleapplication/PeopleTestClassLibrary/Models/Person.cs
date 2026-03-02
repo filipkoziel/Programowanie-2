f7b0695bfcf4a4852a624d2f3cfa431973034a6a
Sala213 <Sala213@ezse.pl>
2026-03-02 15:17:31 +0100

    db fun

---
+++ firsrconsoleapplication/PeopleTestClassLibrary/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeopleTestClassLibrary.Models;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Surname { get; set; } = null!;
+
+    public int Age { get; set; }
+}
